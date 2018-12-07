f0ae9af2907eea1f01548f023ba1fa742bc10812
Egor Panok <egorpanok@gmail.com>
2018-12-07 13:35:39 +0300

    Instrument service -> Fix

--- src/app/core/instrument/instrument.service.ts
+++ src/app/core/instrument/instrument.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ConfigService } from '../common';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
-import { IService, InstrumentModel } from './model/instrument.model';
+import { InstrumentModel } from './model/instrument.model';
 import { HttpHelperService } from "../http-helper.service";
 
 @Injectable()
@@ -13,39 +13,39 @@ export class InstrumentService {
     }
 
     public getAll(): Observable<InstrumentModel[]> {
-        return this._httpHelper.get(true, this._configService.API.Service.default());
+        return this._httpHelper.get(true, this._configService.API.Instrument.default());
     }
 
     public getAllActive(): Observable<InstrumentModel[]> {
-        return this._httpHelper.get(true, this._configService.API.Service.getAllActive());
+        return this._httpHelper.get(true, this._configService.API.Instrument.getAllActive());
     }
 
     public getAllArchived(): Observable<InstrumentModel[]> {
-        return this._httpHelper.get(true, this._configService.API.Service.getAllArchived());
+        return this._httpHelper.get(true, this._configService.API.Instrument.getAllArchived());
     }
 
     public add(model: InstrumentModel): Observable<InstrumentModel> {
-        return this._httpHelper.post(true, this._configService.API.Service.default(), model);
+        return this._httpHelper.post(true, this._configService.API.Instrument.default(), model);
     }
 
     public get(id: string): Observable<InstrumentModel> {
-        return this._httpHelper.get(true, this._configService.API.Service.dafaultWithId(id));
+        return this._httpHelper.get(true, this._configService.API.Instrument.dafaultWithId(id));
     }
 
     public delete(id: string): Observable<InstrumentModel> {
-        return this._httpHelper.delete(true, this._configService.API.Service.dafaultWithId(id));
+        return this._httpHelper.delete(true, this._configService.API.Instrument.dafaultWithId(id));
     }
 
     public update(id: string, model: InstrumentModel): Observable<InstrumentModel> {
-        return this._httpHelper.put(true, this._configService.API.Service.dafaultWithId(id), model);
+        return this._httpHelper.put(true, this._configService.API.Instrument.dafaultWithId(id), model);
     }
 
     public activate(id: string): Observable<InstrumentModel> {
-        return this._httpHelper.get(true, this._configService.API.Service.activate(id));
+        return this._httpHelper.get(true, this._configService.API.Instrument.activate(id));
     }
 
     public archive(id: string): Observable<InstrumentModel> {
-        return this._httpHelper.get(true, this._configService.API.Service.archive(id));
+        return this._httpHelper.get(true, this._configService.API.Instrument.archive(id));
     }
 
 }

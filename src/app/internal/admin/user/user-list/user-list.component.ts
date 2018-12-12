8d36c3db821887884197a245e08cf97fe60c1b86
Egor Panok <egorpanok@gmail.com>
2018-12-12 18:22:50 +0300

    User Search

--- src/app/internal/admin/user/user-list/user-list.component.ts
+++ src/app/internal/admin/user/user-list/user-list.component.ts
@@ -4,6 +4,8 @@ import {
 } from '@angular/core';
 import { UserService } from '../../../../core/user/user.service';
 import { IUser } from "../../../../core/user/model/user.model";
+import { SearchPipe } from '../../../common/search.pipe';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'user-list',
@@ -11,14 +13,23 @@ import { IUser } from "../../../../core/user/model/user.model";
     templateUrl: './user-list.component.html'
 })
 export class UserListComponent implements OnInit {
+
+    form: FormGroup;
     public activeUsers: IUser[] = [];
     public archivedUsers: IUser[] = [];
+    searchPipe: SearchPipe = new SearchPipe();
+    searchFields: string = 'firstName,lastName,email';
 
     constructor(
-        private _userService: UserService
+        private _userService: UserService,
+        private _formBuilder: FormBuilder,
     ) {}
 
     public ngOnInit() {
+        this.form = this._formBuilder.group({
+            search: ['', []]
+        });
+
         // Get active users
         this._userService.getAllActive().subscribe((users: any) => {
             this.activeUsers = users;
@@ -27,5 +38,13 @@ export class UserListComponent implements OnInit {
         this._userService.getAllArchived().subscribe((users: any) => {
             this.archivedUsers = users;
         });
+    }
+
+    public getFilteredActiveUsers() {
+        return this.searchPipe.transform(this.activeUsers, this.searchFields, this.form.controls['search'].value);
+    }
+
+    public getFilteredArchivedUsers() {
+        return this.searchPipe.transform(this.archivedUsers, this.searchFields, this.form.controls['search'].value);
     }
 }

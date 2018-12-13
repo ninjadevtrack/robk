9bc09fbcae6f4663b98d6e81d1709f6b5dfb014d
Egor Panok <egorpanok@gmail.com>
2018-12-13 20:23:59 +0300

    Teacher Add - instruments

--- src/app/internal/admin/teacher/teacher-add/teacher-add.component.ts
+++ src/app/internal/admin/teacher/teacher-add/teacher-add.component.ts
@@ -10,6 +10,8 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { NotSpacesStringValidator } from "../../../../core/validators/not-spaces-string-validator";
 import { AppealService } from '../../../../core/appeal/appeal.service';
 import { UserModel } from '../../../../core/user/model/user.model';
+import { InstrumentService } from '../../../../core/instrument/instrument.service';
+import {IInstrument} from '../../../../core/instrument/model/instrument.model';
 
 @Component({
     selector: 'teacher-add',
@@ -20,9 +22,11 @@ export class TeacherAddComponent implements OnInit {
     form: FormGroup;
     serverErrorMessage: string;
     appeals: string[];
+    instruments: IInstrument[];
 
     constructor(
         private _teacherService: TeacherService,
+        private _instrumentsService: InstrumentService,
         private _appealService: AppealService,
         public dialogRef: MatDialogRef<TeacherAddComponent>,
         private _formBuilder: FormBuilder,
@@ -30,6 +34,9 @@ export class TeacherAddComponent implements OnInit {
 
     ngOnInit() {
 
+        this._instrumentsService.getAllActive().subscribe((instruments: IInstrument[]) => {
+           this.instruments = instruments;
+        });
 
         this._appealService.getAll().subscribe((appeals: string[]) => {
             this.appeals = appeals;
@@ -41,7 +48,8 @@ export class TeacherAddComponent implements OnInit {
                 phone: ['', [Validators.required, Validators.maxLength(60), NotSpacesStringValidator()]],
                 email: ['', [Validators.required, Validators.maxLength(60), NotSpacesStringValidator()]],
                 notes: ['', [Validators.required, Validators.maxLength(3000), NotSpacesStringValidator()]],
-                experience: ['', [Validators.required, Validators.maxLength(3000), NotSpacesStringValidator()]]
+                experience: ['', [Validators.required, Validators.maxLength(3000), NotSpacesStringValidator()]],
+                instruments: ['', [Validators.required, Validators.maxLength(300)]]
             });
         });
     }
@@ -62,6 +70,7 @@ export class TeacherAddComponent implements OnInit {
         const teacherModel = new TeacherModel();
         teacherModel.experience = this.form.controls['experience'].value;
         teacherModel.notes = this.form.controls['notes'].value;
+        teacherModel.instruments = this.form.controls['instruments'].value;
         teacherModel.user = userModel;
 
         return teacherModel;

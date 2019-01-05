9b903e31516429ee0665e93cebc63e3137edb31e
Egor Panok <egorpanok@gmail.com>
2019-01-05 13:43:47 +0300

    Manage: Teacher Calendar > Add / Edit / Remove

--- src/app/internal/admin/teacher/teacher-calendar/teacher-calendar.component.ts
+++ src/app/internal/admin/teacher/teacher-calendar/teacher-calendar.component.ts
@@ -8,6 +8,9 @@ import {StudentService} from '../../../../core/student/student.service';
 import {StudentModel} from '../../../../core/student/model/student.model';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {CalendarColoringModes} from '../../../calendar/utils/calendar-coloring-modes.enum';
+import {IndividualLessonAddEditComponent} from '../../../calendar/individual-lesson/individual-lesson-add-edit/individual-lesson-add-edit.component';
+import {DialogMode} from '../../../../core/common/dialog-mode.enum';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-teacher-calendar',
@@ -16,26 +19,28 @@ import {CalendarColoringModes} from '../../../calendar/utils/calendar-coloring-m
 })
 export class TeacherCalendarComponent implements OnInit {
 
-  filtersForm: FormGroup;
-  id: string;
-  teacher: TeacherModel;
-  students: StudentModel[];
-  individualLessons: IIndividualLesson[];
-  calendarColoringMode: CalendarColoringModes = CalendarColoringModes.BY_TEACHER;
+    filtersForm: FormGroup;
+    id: string;
+    teacher: TeacherModel;
+    students: StudentModel[];
+    individualLessons: IIndividualLesson[];
+    addEditEntityDialogOpened = false;
+    calendarColoringMode: CalendarColoringModes = CalendarColoringModes.BY_TEACHER;
 
-  constructor(
+    constructor(
       private _router: Router,
       private _route: ActivatedRoute,
       private _teacherService: TeacherService,
       private _studentService: StudentService,
       private _individualLessonsService: IndividualLessonService,
-      private _formBuilder: FormBuilder
-  ) { }
+      private _formBuilder: FormBuilder,
+      private _dialog: MatDialog
+    ) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
       this.filtersForm = this._formBuilder.group({
-          students: ['', []]
+          students: [[], []]
       });
 
       this._route.params.subscribe((params) => {
@@ -50,16 +55,54 @@ export class TeacherCalendarComponent implements OnInit {
               this.getIndividualLessons([this.id], []);
           });
       });
-  }
+    }
 
-  getIndividualLessons(teacherIds, studentIds) {
+    getIndividualLessons(teacherIds, studentIds) {
       this._individualLessonsService.search(teacherIds, studentIds).subscribe((individualLessons) => {
           this.individualLessons = individualLessons;
       });
-  }
+    }
 
-  filterSelectionChanged() {
+    getIndividualLessonsBySelectedFilersValues() {
       this.getIndividualLessons([this.id], this.filtersForm.controls['students'].value);
-  }
+    }
+
+    addEditIndividualLesson(data) {
+        if (!this.addEditEntityDialogOpened) {
+            const dialogRef = this._dialog.open(IndividualLessonAddEditComponent, {
+                data: data
+            });
+            this.addEditEntityDialogOpened = true;
+
+            dialogRef.afterClosed().subscribe(result => {
+                if (result) {
+                    this.getIndividualLessonsBySelectedFilersValues();
+                }
+                this.addEditEntityDialogOpened = false;
+            });
+        }
+    }
+
+    addIndividualLesson(event) {
+        const data = {
+            mode: DialogMode.ADD,
+            date: event.date,
+            students: this.students,
+            teachers: [this.teacher]
+        };
+
+        this.addEditIndividualLesson(data);
+    }
+
+    editIndividualLesson(il: IIndividualLesson) {
+        const data = {
+            mode: DialogMode.EDIT,
+            il: il,
+            students: this.students,
+            teachers: [this.teacher]
+        };
+
+        this.addEditIndividualLesson(data);
+    }
 
 }

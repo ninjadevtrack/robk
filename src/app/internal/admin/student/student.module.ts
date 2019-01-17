b55c69296d0fa381b21c7ec02fee296988bb9252
Egor Panok <egorpanok@gmail.com>
2019-01-17 18:44:19 +0300

    Cleaning AppModule

--- src/app/internal/admin/student/student.module.ts
+++ src/app/internal/admin/student/student.module.ts
@@ -5,31 +5,33 @@ import { RouterModule} from '@angular/router';
 import { InternalCommonModule} from '../../common/internal-common.module';
 import { ReactiveFormsModule} from '@angular/forms';
 import {
-  MatButtonModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatOptionModule,
-  MatSelectModule
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule
 } from '@angular/material';
-import {AdminStudentCalendarComponent} from './admin-student-calendar/admin-student-calendar.component';
-import {StudentAddComponent} from './student-add/student-add.component';
-import {StudentEditComponent} from './student-edit/student-edit.component';
-import {StudentListComponent} from './student-list/student-list.component';
-import {StudentListRootComponent} from './student-list-root/student-list-root.component';
-import {StudentViewComponent} from './student-view/student-view.component';
+import { AdminStudentCalendarComponent } from './admin-student-calendar/admin-student-calendar.component';
+import { StudentAddComponent } from './student-add/student-add.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentListRootComponent } from './student-list-root/student-list-root.component';
+import { StudentViewComponent } from './student-view/student-view.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    CoreModule,
-    RouterModule,
-    InternalCommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatOptionModule
+        CommonModule,
+        CoreModule,
+        RouterModule,
+        InternalCommonModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatDialogModule
   ],
   declarations: [
       AdminStudentCalendarComponent,

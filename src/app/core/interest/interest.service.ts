cd58ad61612a877b9341f082c391e30983fda4b9
Egor Panok <egorpanok@gmail.com>
2019-02-07 00:15:33 +0300

    New interests

--- src/app/core/interest/interest.service.ts
+++ src/app/core/interest/interest.service.ts
@@ -13,7 +13,17 @@ export class InterestService {
     { id: 2, name: 'Gardening' },
     { id: 3, name: 'Makeup' },
     { id: 4, name: 'Sport' },
-    { id: 5, name: 'IT' }
+    { id: 5, name: 'IT' },
+    { id: 6, name: 'Art' },
+    { id: 7, name: 'Politics' },
+    { id: 8, name: 'USA' },
+    { id: 9, name: 'Russia' },
+    { id: 10, name: 'Geography' },
+    { id: 11, name: 'History' },
+    { id: 12, name: 'Math' },
+    { id: 13, name: 'Biology' },
+    { id: 14, name: 'Physics' },
+    { id: 15, name: 'Economics' }
   ];
 
   constructor() { }

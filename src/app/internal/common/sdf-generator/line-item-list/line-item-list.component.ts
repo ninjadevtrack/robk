0b1515084b9972df66f4cb2a050491a99f8aeccf
Egor Panok <egorpanok@gmail.com>
2019-02-06 23:34:38 +0300

    Paging

--- src/app/internal/common/sdf-generator/line-item-list/line-item-list.component.ts
+++ src/app/internal/common/sdf-generator/line-item-list/line-item-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input, ViewChild} from '@angular/core';
 import { ILineItem } from "../../../../core/line-item/i-line-item";
 import { MatPaginator, MatTableDataSource } from "@angular/material";
+import {IComment} from "../../../../core/common/models/comment.model";
 
 @Component({
   selector: 'app-line-item-list',
@@ -9,17 +10,25 @@ import { MatPaginator, MatTableDataSource } from "@angular/material";
 })
 export class LineItemListComponent implements OnInit {
 
-  @Input() lineItems: ILineItem[];
+  _lineItems: ILineItem[];
   displayedColumns: string[] = ['campaignName', 'device', 'geo', 'interest', 'gender', 'ageCategory'];
   dataSource: MatTableDataSource<ILineItem>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
+  @Input() set lineItems(value: ILineItem[]) {
+    this._lineItems = value;
+    this.rebuildDataTable();
+  }
+
   ngOnInit() {
+  }
+
+  rebuildDataTable() {
     console.log(`Rendering begins ${(new Date()).toISOString()}`);
-    this.dataSource = new MatTableDataSource<ILineItem>(this.lineItems);
-    setTimeout(() => { this.dataSource.paginator = this.paginator; }, 1);
+    this.dataSource = new MatTableDataSource<ILineItem>(this._lineItems);
+    setTimeout(() => { this.dataSource.paginator = this.paginator; }, 100);
     console.log(`Rendering ends ${(new Date()).toISOString()}`);
   }
 

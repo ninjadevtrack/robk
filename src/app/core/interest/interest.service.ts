e6e6acbef0268b447c989f01231d5460147409a5
Egor Panok <egorpanok@gmail.com>
2019-02-08 15:12:09 +0300

    Interests went to JSON

--- src/app/core/interest/interest.service.ts
+++ src/app/core/interest/interest.service.ts
@@ -1,34 +1,16 @@
 import { Injectable } from '@angular/core';
 import { IInterest } from "./interest";
-import { Observable, of } from "rxjs";
-
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterestService {
 
-  interests: IInterest[] = [
-    { id: 1, name: 'Parenting' },
-    { id: 2, name: 'Gardening' },
-    { id: 3, name: 'Makeup' },
-    { id: 4, name: 'Sport' },
-    { id: 5, name: 'IT' },
-    { id: 6, name: 'Art' },
-    { id: 7, name: 'Politics' },
-    { id: 8, name: 'USA' },
-    { id: 9, name: 'Russia' },
-    { id: 10, name: 'Geography' },
-    { id: 11, name: 'History' },
-    { id: 12, name: 'Math' },
-    { id: 13, name: 'Biology' },
-    { id: 14, name: 'Physics' },
-    { id: 15, name: 'Economics' }
-  ];
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getAll(): Observable<IInterest[]> {
-    return of(this.interests);
+    return this.http.get<IInterest[]>("./assets/data/interests.json");
   }
 }

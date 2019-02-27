92580e08b4f9872e87213b57d2c26b4738fb7d53
Egor Panok <egorpanok@gmail.com>
2019-02-27 15:59:09 +0300

    Tags filter

--- src/app/internal/common/graph-watch-list/graph-watch-list.component.ts
+++ src/app/internal/common/graph-watch-list/graph-watch-list.component.ts
@@ -8,6 +8,7 @@ import { fromMatSort, sortRows} from './../../../core/datasource-utils';
 
 import {GraphWatchListService} from "../../../core/graph-watch-list/graph-watch-list.service";
 import {ICompany, ICompanyValue} from "../../../core/graph-watch-list/model/company.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-graph-watch-list',
@@ -18,17 +19,40 @@ export class GraphWatchListComponent implements OnInit {
 
   @ViewChild(MatSort) sort: MatSort;
   companies$: Observable<ICompany[]>;
+  tags: string[] = [];
   sortedCompanyValues$: Observable<ICompanyValue[]>;
+  form: FormGroup;
 
   constructor(
-      private _graphWatchlistService: GraphWatchListService
+      private _graphWatchlistService: GraphWatchListService,
+      private _formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
+
+    this.form = this._formBuilder.group({
+      tags: [[], []]
+    });
+
     const sortEvents$: Observable<Sort> = fromMatSort(this.sort);
     this.companies$ = this._graphWatchlistService.getCompanies();
     this.sortedCompanyValues$ = this.companies$.pipe(
         map(co => co.map(c => c.value)), sortRows(sortEvents$));
+
+    // Let's collect all unique tags
+    this.companies$.subscribe((cmps: ICompany[]) => {
+        let tags;
+        cmps.forEach(cmp => {
+          tags = cmp.value.tags.split(',');
+          tags = tags.map(t => t.trim());
+          tags.forEach(tag => {
+            if (tag && !this.tags.includes(tag)) {
+              this.tags.push(tag);
+            }
+          });
+        });
+        this.tags.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+    });
   }
 
 }

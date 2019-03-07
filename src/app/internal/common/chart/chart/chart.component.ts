a9fd0ee531befb5d0d155e0a1fd27a811c144d3c
Egor Panok <egorpanok@gmail.com>
2019-03-07 15:56:08 +0300

    Fixing a great bug

--- src/app/internal/common/chart/chart/chart.component.ts
+++ src/app/internal/common/chart/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild, Input} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, Input, AfterViewInit} from '@angular/core';
 import {Chart} from 'chart.js';
 import {ICompany} from "../../../../core/graph-watch-list/model/company.model";
 import * as moment from 'moment';
@@ -8,48 +8,51 @@ import * as moment from 'moment';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, AfterViewInit {
 
   @ViewChild('canvas') canvas: ElementRef;
   @Input() company: ICompany;
   chart: any = [];
+  data: any;
 
   constructor() {}
 
   ngOnInit() {
 
-    setTimeout(() => {
-      this.chart = new Chart(`canvas_${this.company.name}`, {
-          type: "line",
-          data: {
-            labels: this.company.data.map((d) => moment(d[0]).format('MM/YY')),
-            datasets: [{
-              label: this.company.name,
-              data: this.company.data.map((d) => d[1]),
-              fill: false,
-              borderColor: 'rgb(75, 192, 192)',
-              lineTension: 0.1
-            }]
-          },
-          options: {
-            animation: false,
-            maintainAspectRatio: false,
-            scales: {
-              yAxes: [{
-                display: true,
-                ticks: {
-                  suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
-                }
-              }]
-            },
-            chartArea: {
-              backgroundColor: this.getColor()
+    this.data = {
+      type: "line",
+      data: {
+        labels: this.company.data.map((d) => moment(d[0]).format('MM/YY')),
+        datasets: [{
+          label: this.company.name,
+          data: this.company.data.map((d) => d[1]),
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          lineTension: 0.1
+        }]
+      },
+      options: {
+        animation: false,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+              suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
             }
-          }
+          }]
+        },
+        chartArea: {
+          backgroundColor: this.getColor()
         }
-      );
-    }, 10);
+      }
+    };
+  }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.chart = new Chart(this.canvas.nativeElement.getContext('2d'), this.data);
+    }, 0);
   }
 
   getColor() {

8845b9932ba3c4fd8478e960ef95dd3398afecb3
Egor Panok <egorpanok@gmail.com>
2019-03-25 12:50:36 +0300

    Navigate to internal page if user is logged in

--- src/app/public/public-entry.component.ts
+++ src/app/public/public-entry.component.ts
@@ -3,27 +3,38 @@ import {
     OnInit
 } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from "../core/auth/auth.service";
 
 @Component({
     selector: 'robscore-public-entry',
     providers: [
     ],
     template: `
-        <div class="m-b-2xl text-center m-t-6xl">
-            <a routerLink="/"><img [src]="logo" height="82px"></a>
-        </div>
-        <router-outlet></router-outlet>`
+        <div *ngIf="showForm">
+            <div class="m-b-2xl text-center m-t-6xl">
+                <a routerLink="/"><img [src]="logo" height="82px"></a>
+            </div>
+            <router-outlet></router-outlet>
+        </div>`
 })
 export class PublicEntryComponent implements OnInit {
     public logo = 'assets/img/logo-dark.svg';
-    public name = 'Teachable';
+    public name = 'RobScore';
+    public showForm: boolean = false;
 
     /**
      * TypeScript public modifiers
      */
     constructor(
+        private _authService: AuthService,
         private _router: Router
-    ) {}
+    ) {
+        this._authService.validateToken().subscribe(res => {
+            this._router.navigateByUrl('/i');
+        }, (err) => {
+            this.showForm = true;
+        });
+    }
 
     public ngOnInit() {
     }

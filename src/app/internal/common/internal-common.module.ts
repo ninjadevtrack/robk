aabf782294a8cf362ad128b284ac95506b330cae
Egor Panok <egorpanok@gmail.com>
2019-08-23 20:22:38 +0300

    Company service now is a provider of the CoreModule

--- src/app/internal/common/internal-common.module.ts
+++ src/app/internal/common/internal-common.module.ts
@@ -1,74 +1,71 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProfileComponent } from './profile/profile.component';
-import { InternalHeaderComponent } from './header/header.component';
-import { InternalCoreModule } from '../core/internal-core.module';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProfileComponent } from "./profile/profile.component";
+import { InternalHeaderComponent } from "./header/header.component";
+import { InternalCoreModule } from "../core/internal-core.module";
+import { RouterModule } from "@angular/router";
 import {
     MatAutocompleteModule,
     MatButtonModule,
-    MatCardModule, MatCheckboxModule, MatExpansionModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule, MatListModule,
+    MatInputModule,
+    MatListModule,
     MatMenuModule,
     MatProgressSpinnerModule,
-    MatSelectModule, MatSortModule,
+    MatSelectModule,
+    MatSortModule,
     MatTableModule,
     MatToolbarModule
-} from '@angular/material';
+} from "@angular/material";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { ReactiveFormsModule } from "@angular/forms";
-import { CompaniesComponent } from './companies/companies.component';
-import { CompanyService } from "../../core/company/company.service";
-import { ChartComponent } from './chart/chart/chart.component';
+import { CompaniesComponent } from "./companies/companies.component";
+import { ChartComponent } from "./chart/chart/chart.component";
 import { InViewportModule } from "ng-in-viewport";
 import { CompanyDetailsComponent } from "./company-details/company-details.component";
-import { ActivityFeedComponent } from './activity-feed/activity-feed.component';
+import { ActivityFeedComponent } from "./activity-feed/activity-feed.component";
+import { CoreModule } from "src/app/core/core.module";
 
 @NgModule({
-  imports: [
-      CommonModule,
-      RouterModule,
-      MatMenuModule,
-      MatToolbarModule,
-      MatFormFieldModule,
-      MatButtonModule,
-      MatInputModule,
-      MatSelectModule,
-      MatIconModule,
-      MatCardModule,
-      MatProgressSpinnerModule,
-      MatPaginatorModule,
-      MatTableModule,
-      MatListModule,
-      MatExpansionModule,
-      MatSortModule,
-      MatCheckboxModule,
-      MatProgressSpinnerModule,
-      MatAutocompleteModule,
-      InternalCoreModule,
-      ReactiveFormsModule,
-      InViewportModule
-  ],
-  declarations: [
-      ProfileComponent,
-      InternalHeaderComponent,
-      CompaniesComponent,
-      ChartComponent,
-      CompanyDetailsComponent,
-      ActivityFeedComponent
-  ],
-  exports: [
-      ProfileComponent,
-      InternalHeaderComponent
-  ],
-  providers: [
-      CompanyService
-  ]
-
+    imports: [
+        CommonModule,
+        RouterModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatInputModule,
+        MatSelectModule,
+        MatIconModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatPaginatorModule,
+        MatTableModule,
+        MatListModule,
+        MatExpansionModule,
+        MatSortModule,
+        MatCheckboxModule,
+        MatProgressSpinnerModule,
+        MatAutocompleteModule,
+        InternalCoreModule,
+        ReactiveFormsModule,
+        InViewportModule,
+        CoreModule
+    ],
+    declarations: [
+        ProfileComponent,
+        InternalHeaderComponent,
+        CompaniesComponent,
+        ChartComponent,
+        CompanyDetailsComponent,
+        ActivityFeedComponent
+    ],
+    exports: [ProfileComponent, InternalHeaderComponent]
 })
-export class InternalCommonModule { }
-
+export class InternalCommonModule {}
 
 /*BrowserModule, FormsModule, MatListModule, MatExpansionModule, MatSortModule, BrowserAnimationsModule, FlexLayoutModule, MatPaginatorModule*/

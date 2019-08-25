910e74330d63073480d238d23183d42b16816f8e
Egor Panok <egorpanok@gmail.com>
2019-08-25 16:49:13 +0300

    ActivityFeedComponent initial commit

--- src/app/internal/common/activity-feed/activity-feed.component.ts
+++ src/app/internal/common/activity-feed/activity-feed.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivityFeedService } from "src/app/core/activity-feed/activity-feed.service";
 
 @Component({
-  selector: 'app-activity-feed',
-  templateUrl: './activity-feed.component.html',
-  styleUrls: ['./activity-feed.component.scss']
+    selector: "app-activity-feed",
+    templateUrl: "./activity-feed.component.html",
+    styleUrls: ["./activity-feed.component.scss"]
 })
 export class ActivityFeedComponent implements OnInit {
+    constructor(private _activityFeedService: ActivityFeedService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this._activityFeedService.getFeed(100, 0).subscribe(activities => {
+            console.log(activities);
+        });
+    }
 }

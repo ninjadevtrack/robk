41d689007cfab023bcd539097d4090897acb46ca
Egor Panok <egorpanok@gmail.com>
2020-07-03 15:42:46 +0300

    Don't show chart legends

--- src/app/internal/internal-entry.component.ts
+++ src/app/internal/internal-entry.component.ts
@@ -1,21 +1,18 @@
-import {
-    Component,
-    OnInit
-} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "chart.js";
 
 @Component({
-    selector: 'robscore-internal-entry',
-    providers: [
-    ],
+    selector: "robscore-internal-entry",
+    providers: [],
     template: `
         <robscore-internal-header></robscore-internal-header>
-		<router-outlet></router-outlet>
+        <router-outlet></router-outlet>
     `
 })
 export class InternalEntryComponent implements OnInit {
-
-    constructor() {}
-
-    public ngOnInit() {
+    constructor() {
+        Chart.defaults.global.legend.display = false;
     }
+
+    public ngOnInit() {}
 }

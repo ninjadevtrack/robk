7415d8e14aa65ff21884b14b70a345c5a8ed6565
Egor Panok <egorpanok@gmail.com>
2020-07-04 13:30:13 +0300

    Config => Company - minor change

--- src/app/core/common/config.service.ts
+++ src/app/core/common/config.service.ts
@@ -85,7 +85,7 @@ export class ConfigService {
                 return `${Consts.API_URL}/v1/companies`;
             },
             getByLIUrlSlug: urlSlug => {
-                return `${Consts.API_URL}/v1/companies/slug/${urlSlug}`;
+                return `${Consts.API_URL}/v1/companies/rs/${urlSlug}`;
             },
             toggleIgnore: id => {
                 return `${Consts.API_URL}/v1/companies/${id}/toggle-ignore`;

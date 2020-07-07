5cc3164c08cc7bd53c1931533c5bdb2ef7c1a5e3
Egor Panok <egorpanok@gmail.com>
2020-07-07 17:38:25 +0300

    CapsuleWebsites

--- src/app/internal/common/company/capsule-websites/capsule-websites.component.ts
+++ src/app/internal/common/company/capsule-websites/capsule-websites.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { ICapsuleDetails } from "src/app/core/company/model/capsule-details.model";
+import {
+    ICapsuleDetails,
+    ICapsuleDetailsWebsite
+} from "src/app/core/company/model/capsule-details.model";
 
 @Component({
     selector: "app-capsule-websites",
@@ -8,7 +11,23 @@ import { ICapsuleDetails } from "src/app/core/company/model/capsule-details.mode
 })
 export class CapsuleWebsitesComponent implements OnInit {
     @Input() capsuleDetails: ICapsuleDetails;
+    websites: ICapsuleDetailsWebsite[];
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        const liType = "LINKED_IN";
+
+        this.websites = this.capsuleDetails.websites.sort((a, b) => {
+            if (a.service === liType && b.service !== liType) {
+                return -1;
+            }
+            if (a.service === b.service) {
+                return 0;
+            }
+
+            return 1;
+        });
+    }
+
+    getWebsites() {}
 }

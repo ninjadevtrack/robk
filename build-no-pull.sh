87664f79fff635295e1985003b9b26db28977ce3
a10kiloham <295361+a10kiloham@users.noreply.github.com>
2020-08-11 12:03:13 +0100

    fix build script

--- build-no-pull.sh
+++ build-no-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
     rm -rf dist
-    ng build --prod
     npm i
+    npx ng build --prod
     find "/var/www/dev.hoxtonventures.com/public_html" -type f -mtime +5 -name 'main.*.js' -delete
     find "/var/www/dev.hoxtonventures.com/public_html" -type f -mtime +5 -name 'runtime.*.js' -delete
     find "/var/www/dev.hoxtonventures.com/public_html" -type f -mtime +5 -name '4.*.js' -delete

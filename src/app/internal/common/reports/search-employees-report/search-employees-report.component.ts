d6c0b755fdedca664c3caf0f44e40afec0979e45
Egor Panok <egorpanok@gmail.com>
2021-11-19 18:10:56 +0100

    Search Employees by two fields

--- src/app/internal/common/reports/search-employees-report/search-employees-report.component.ts
+++ src/app/internal/common/reports/search-employees-report/search-employees-report.component.ts
@@ -5,6 +5,7 @@ import { SmoothScrollService } from "src/app/core/smooth-scroll.service";
 import { ISearchEmployeesEntity } from 'src/app/core/reports/models/search-employees.model';
 import { NotSpacesStringValidator } from 'src/app/core/validators/not-spaces-string-validator';
 import * as moment from "moment";
+import { EmployeeSearchField } from 'src/app/core/reports/models/search-employees-params.model';
 @Component({
   selector: 'app-search-employees-report',
   templateUrl: './search-employees-report.component.html',
@@ -15,6 +16,10 @@ export class SearchEmployeesReportComponent implements OnInit {
     form: FormGroup;
     isDataLoading: boolean;
     isExporting: boolean;
+    searchFields: EmployeeSearchField[] = [
+        EmployeeSearchField.HISTORY_DESCRIPTION,
+        EmployeeSearchField.OCCUPATION
+    ];
 
     constructor(
         private _formBuilder: FormBuilder,
@@ -26,6 +31,7 @@ export class SearchEmployeesReportComponent implements OnInit {
         this.isDataLoading = false;
         this.isExporting = false;
         this.form = this._formBuilder.group({
+            searchField: [EmployeeSearchField.HISTORY_DESCRIPTION, [Validators.required]],
             search: ["", [
                 Validators.required,
                 NotSpacesStringValidator()
@@ -45,7 +51,8 @@ export class SearchEmployeesReportComponent implements OnInit {
         this.isDataLoading = true;
         this.entities = null;
         const regex = this.form.controls["search"].value;
-        const model = {regex, pageSize: 100, pageNumber: 1};
+        const searchField = this.form.controls["searchField"].value;
+        const model = { regex, searchField, pageSize: 100, pageNumber: 1 };
         this._peopleWatchService
             .searchEmployees(model)
             .subscribe((entities: ISearchEmployeesEntity[]) => {
@@ -61,38 +68,44 @@ export class SearchEmployeesReportComponent implements OnInit {
         const suffix = moment().format("MM-DD-YYYY-hh_mm_ss");
         const fileName = `Employees_Search_${suffix}.xlsx`;
         const regex = this.form.controls["search"].value;
-        this._peopleWatchService.searchEmployeesExportToXLSX(regex)
-        .subscribe(x => {
-            // It is necessary to create a new blob object with mime-type explicitly set
-            // otherwise only Chrome works like it should
-            var newBlob = new Blob([x], { type: "application/vnd.ms-excel" });
-            
-            // IE doesn't allow using a blob object directly as link href
-            // instead it is necessary to use msSaveOrOpenBlob
-            // @ts-ignore
-            if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        const searchField = this.form.controls["searchField"].value;
+        const model = { regex, searchField };
+        this._peopleWatchService.searchEmployeesExportToXLSX(model)
+            .subscribe(x => {
+                // It is necessary to create a new blob object with mime-type explicitly set
+                // otherwise only Chrome works like it should
+                var newBlob = new Blob([x], { type: "application/vnd.ms-excel" });
+                
+                // IE doesn't allow using a blob object directly as link href
+                // instead it is necessary to use msSaveOrOpenBlob
                 // @ts-ignore
-                window.navigator.msSaveOrOpenBlob(newBlob, fileName);
-                return;
-            }
-            
-            // For other browsers: 
-            // Create a link pointing to the ObjectURL containing the blob.
-            const data = window.URL.createObjectURL(newBlob);
-            
-            var link = document.createElement('a');
-            link.href = data;
-            link.download = fileName;
-            // this is necessary as link.click() does not work on the latest firefox
-            link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
-            
-            setTimeout(function () {
-                // For Firefox it is necessary to delay revoking the ObjectURL
-                window.URL.revokeObjectURL(data);
-                link.remove();
-            }, 100);
+                if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+                    // @ts-ignore
+                    window.navigator.msSaveOrOpenBlob(newBlob, fileName);
+                    return;
+                }
+                
+                // For other browsers: 
+                // Create a link pointing to the ObjectURL containing the blob.
+                const data = window.URL.createObjectURL(newBlob);
+                
+                var link = document.createElement('a');
+                link.href = data;
+                link.download = fileName;
+                // this is necessary as link.click() does not work on the latest firefox
+                link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+                
+                setTimeout(function () {
+                    // For Firefox it is necessary to delay revoking the ObjectURL
+                    window.URL.revokeObjectURL(data);
+                    link.remove();
+                }, 100);
 
-            this.isExporting = false;
-        });
+                this.isExporting = false;
+            });
+    }
+
+    getEmployeeSearchFieldName(searchField: EmployeeSearchField) {
+        return this._peopleWatchService.getEmployeeSearchFieldName(searchField);
     }
 }

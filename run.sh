8c6867db1abaee5c03355d8c801506af9ef53bae
a10kiloham <295361+a10kiloham@users.noreply.github.com>
2024-02-07 15:56:30 -0500

    Before chart change

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 DATE=$(date --iso-8601)
 sudo rm -rf dist
 npm run build && \
-sudo mkdir -p /var/html/dev.hoxtonventures.com/$DATE && \
+#sudo mkdir -p /var/html/dev.hoxtonventures.com/$DATE && \
+sudo rm -rf /var/html/dev.hoxtonventures.com/$DATE && \
 sudo mv /var/html/dev.hoxtonventures.com/public_html/* /var/html/dev.hoxtonventures.com/$DATE && \
 sudo cp -R ./dist/robscore-web/* /var/html/dev.hoxtonventures.com/public_html && \
 sudo chmod -R 777 /var/html/dev.hoxtonventures.com/public_html
